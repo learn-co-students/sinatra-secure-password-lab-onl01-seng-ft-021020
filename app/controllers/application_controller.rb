1e657561002958bbf205174b859a0e6852bee025
github username <github email address>
2020-03-28 18:05:14 -0700

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -18,7 +18,15 @@ class ApplicationController < Sinatra::Base
 
   post "/signup" do
     #your code here
-
+    if params[:username]=="" || params[:password]==""
+      redirect to'/failure'
+    end
+    user = User.new(:username => params[:username], :password => params[:password])
+    if user.save
+      redirect to "/login"
+    else
+      redirect to "/failure"
+    end
   end
 
   get '/account' do
@@ -33,6 +41,14 @@ class ApplicationController < Sinatra::Base
 
   post "/login" do
     ##your code here
+    user = User.find_by(:username => params[:username])
+
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect to "/account"
+    else
+      redirect to "/failure"
+    end
   end
 
   get "/failure" do
