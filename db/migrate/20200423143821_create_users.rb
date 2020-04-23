9c8ec8abf155b9d2efc75dc59fea33b2dab05a40
Steven Perfors <stevecanbuild@gmail.com>
2020-04-23 21:24:53 +0000

    Done.

--- db/migrate/20200423143821_create_users.rb
+++ db/migrate/20200423143821_create_users.rb
@@ -2,7 +2,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
   def change
     create_table :users do |x|
       x.string :username
-      x.string :password 
+      x.string :password_digest 
     end
   end
 end
